2ee5bb3e0a959c9755f2606244343fef55f597c5
odi Ehimuan <149160848+isharpz007@users.noreply.github.com>
2025-03-24 08:43:53 +0000

    testing for the auth page register and login

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,47 +1,27 @@
-@extends('layouts.auth')
-
-@section('content')
-    <h4 class="fw-bold">Confirm Password</h4>
-    <p class="mb-0">This is a secure area of the application. Please confirm your password before continuing.</p>
-
-    <div class="form-body mt-4">
-        <form method="POST" action="{{ route('password.confirm') }}" class="row g-3">
-            @csrf
-
-            <!-- Password -->
-            <div class="col-12">
-                <label for="password" class="form-label">Password</label>
-                <div class="input-group" id="show_hide_password">
-                    <input type="password" class="form-control" id="password" name="password" required
-                        autocomplete="current-password" placeholder="Enter your password">
-                    <a href="javascript:;" class="input-group-text bg-transparent"><i class="bi bi-eye-slash-fill"></i></a>
-                </div>
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Submit Button -->
-            <div class="col-12">
-                <div class="d-grid">
-                    <button type="submit" class="btn btn-grd-primary">Confirm</button>
-                </div>
-            </div>
-        </form>
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600">
+        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
     </div>
 
-    <script>
-        $(document).ready(function() {
-            $("#show_hide_password a").on('click', function(event) {
-                event.preventDefault();
-                if ($('#show_hide_password input').attr("type") == "text") {
-                    $('#show_hide_password input').attr('type', 'password');
-                    $('#show_hide_password i').addClass("bi-eye-slash-fill");
-                    $('#show_hide_password i').removeClass("bi-eye-fill");
-                } else if ($('#show_hide_password input').attr("type") == "password") {
-                    $('#show_hide_password input').attr('type', 'text');
-                    $('#show_hide_password i').removeClass("bi-eye-slash-fill");
-                    $('#show_hide_password i').addClass("bi-eye-fill");
-                }
-            });
-        });
-    </script>
-@endsection
+    <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
+
+        <!-- Password -->
+        <div>
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <div class="flex justify-end mt-4">
+            <x-primary-button>
+                {{ __('Confirm') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
