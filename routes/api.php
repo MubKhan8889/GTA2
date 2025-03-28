0af2284a27d9daf1f7b45c9cb00f78abf46aac72
Yonathan Hagos <164093649+yon-55@users.noreply.github.com>
2025-03-28 14:01:53 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
+<?php
+
+use App\Http\Controllers\ApprenticeshipController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/apprenticeships', [ApprenticeshipController::class, 'getApprenticeships']);
+Route::get('/apprentices', [ApprenticeshipController::class, 'getApprentices']);
+Route::post('/assign', [ApprenticeshipController::class, 'assignApprenticeship']);
 
